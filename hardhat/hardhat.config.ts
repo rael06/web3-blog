82075d362e6c28854f1db06a72f37ab8924949d0
Rael CALITRO <rael06@hotmail.fr>
2025-01-31 09:37:53 +0100

    Add dotenv for environment variable management and configure Sepolia network in Hardhat

--- hardhat/hardhat.config.ts
+++ hardhat/hardhat.config.ts
@@ -1,5 +1,8 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
+import * as dotenv from "dotenv";
+
+dotenv.config();
 
 const config: HardhatUserConfig = {
   solidity: "0.8.28",
@@ -7,6 +10,13 @@ const config: HardhatUserConfig = {
     hardhat: {
       chainId: 1337,
     },
+    sepolia: {
+      url: process.env.SEPOLIA_RPC_URL!,
+      accounts: [process.env.SEPOLIA_BLOG_CONTRACT_OWNER_PK!],
+    },
+  },
+  etherscan: {
+    apiKey: process.env.ETHERSCAN_API_KEY!,
   },
 };
 
