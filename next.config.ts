31a1afa495167845c396344e84f4f49551b5e3fa
Rael CALITRO <rael06@hotmail.fr>
2025-02-01 16:45:08 +0100

    Remove CORS headers configuration from Next.js config

--- next.config.ts
+++ next.config.ts
@@ -1,31 +1,7 @@
-import { serverEnvVars } from "@/app/services/serverEnvVars";
-import { NextConfig } from "next";
+import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  async headers() {
-    return [
-      {
-        // Apply these headers to all API routes
-        source: "/api/:path*",
-        headers: [
-          { key: "Access-Control-Allow-Credentials", value: "true" },
-          {
-            key: "Access-Control-Allow-Origin",
-            value: serverEnvVars.CORS_ORIGIN,
-          },
-          {
-            key: "Access-Control-Allow-Methods",
-            value: "GET,DELETE,PATCH,POST,PUT,OPTIONS",
-          },
-          {
-            key: "Access-Control-Allow-Headers",
-            value:
-              "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version",
-          },
-        ],
-      },
-    ];
-  },
+  /* config options here */
 };
 
 export default nextConfig;
--- src/app/services/serverEnvVars.ts
+++ src/app/services/serverEnvVars.ts
@@ -3,7 +3,6 @@ import { z } from "zod";
 const schema = z.object({
   NEXT_PUBLIC_BLOG_CONTRACT_ADDRESS: z.string(),
   NEXT_PUBLIC_BLOCKCHAIN_RPC_URL: z.string(),
-  NEXT_PUBLIC_CORS_ORIGIN: z.string(),
   NEXT_IPFS_API_URL: z.string(),
   NEXT_PUBLIC_IPFS_GET_URL: z.string(),
   NEXT_IS_IPFS_PIN_ENABLED: z.string(),
@@ -14,7 +13,6 @@ function getEnvVars() {
   return {
     BLOG_CONTRACT_ADDRESS: env.NEXT_PUBLIC_BLOG_CONTRACT_ADDRESS,
     BLOCKCHAIN_RPC_URL: env.NEXT_PUBLIC_BLOCKCHAIN_RPC_URL,
-    CORS_ORIGIN: env.NEXT_PUBLIC_CORS_ORIGIN,
     IPFS_API_URL: env.NEXT_IPFS_API_URL,
     IPFS_GET_URL: env.NEXT_PUBLIC_IPFS_GET_URL,
     IS_IPFS_PIN_ENABLED: env.NEXT_IS_IPFS_PIN_ENABLED === "true",
