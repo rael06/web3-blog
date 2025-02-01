f4e87b7c98c60f19141333345d8b8accfe57c509
Rael CALITRO <rael06@hotmail.fr>
2025-02-01 19:22:03 +0100

    Remove commented-out logging in addData function and streamline IPFS connection code

--- src/app/api/ipfs.ts
+++ src/app/api/ipfs.ts
@@ -3,21 +3,17 @@ import "server-only";
 import { create } from "kubo-rpc-client";
 import { serverEnvVars } from "@/app/services/serverEnvVars";
 
-// Connect to your local IPFS node
-const ipfs = create({ url: serverEnvVars.IPFS_API_URL }); // Adjust the URL if your IPFS node is hosted elsewhere
+const ipfs = create({ url: serverEnvVars.IPFS_API_URL });
 
-// Function to add data to IPFS
 export async function addData(
   data: Record<string, unknown> | Record<string, unknown>[]
 ): Promise<string> {
-  console.log(JSON.stringify(serverEnvVars));
   const { cid } = await ipfs.add(JSON.stringify(data));
   if (serverEnvVars.IS_IPFS_PIN_ENABLED) await ipfs.pin.add(cid);
   console.log("Data added with CID:", cid.toString());
   return cid.toString();
 }
 
-// Function to retrieve data from IPFS
 export async function getData(cid: string): Promise<unknown> {
   const decoder = new TextDecoder();
   let content = "";
