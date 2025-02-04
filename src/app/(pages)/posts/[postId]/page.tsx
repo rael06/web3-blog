5d5c81b3bb7a630cb1f91c606c692ba5a62d857f
Rael CALITRO <rael06@hotmail.fr>
2025-02-04 13:49:17 +0100

    Refactor post page layout to improve readability and add post ID and IPFS CID display

--- src/app/(pages)/posts/[postId]/page.tsx
+++ src/app/(pages)/posts/[postId]/page.tsx
@@ -2,14 +2,7 @@ import ImageWithFallback from "@/app/components/ImageWithFallback";
 import { formatDate } from "@/app/services/formatters";
 import { serverEnvVars } from "@/app/services/serverEnvVars";
 import { fetchPosts } from "@/usecases/blog/fetchPosts";
-import {
-  Card,
-  CardActionArea,
-  CardContent,
-  Typography,
-  Box,
-  Container,
-} from "@mui/material";
+import { Card, CardContent, Typography, Box, Container } from "@mui/material";
 import Link from "next/link";
 
 export const revalidate = 3600;
@@ -41,36 +34,42 @@ export default async function Page({
   }
   return (
     <Container>
-      <Card key={post.id} sx={{ m: 2 }}>
-        <CardActionArea
-          component={Link}
-          href={`/posts/${post.id}`}
-          sx={{ p: 2 }}
-        >
-          <ImageWithFallback
-            src={
-              post.content.imageCid
-                ? `${serverEnvVars.IPFS_GET_URL}/${post.content.imageCid}`
-                : "/no-image-fallback.png"
-            }
-            fallbackSrc="/ipfs-in-progress-fallback.png"
-            alt={post.content.title}
-            sx={{ height: "400px", objectFit: "contain" }}
-          />
-          <CardContent>
-            <Typography gutterBottom variant="h5" component="div">
-              {post.content.title}
-            </Typography>
-            <Typography variant="body2">{post.content.body}</Typography>
-            <Typography variant="body2" mt={2}>
-              Category: {post.category}
-            </Typography>
-            <Typography variant="body2">By {post.authorAddress}</Typography>
-            <Typography variant="body2">
-              {formatDate(post.createdAt)}
-            </Typography>
-          </CardContent>
-        </CardActionArea>
+      <Card key={post.id} sx={{ m: 2, p: 2 }}>
+        <ImageWithFallback
+          src={
+            post.content.imageCid
+              ? `${serverEnvVars.IPFS_GET_URL}/${post.content.imageCid}`
+              : "/no-image-fallback.png"
+          }
+          fallbackSrc="/ipfs-in-progress-fallback.png"
+          alt={post.content.title}
+          sx={{ height: "400px", objectFit: "contain" }}
+        />
+        <CardContent>
+          <Typography gutterBottom variant="h5" component="div">
+            {post.content.title}
+          </Typography>
+          <Typography variant="body1" mt={2}>
+            {post.content.body}
+          </Typography>
+          <Typography variant="body2" mt={2}>
+            Post ID: {post.id}
+          </Typography>
+          <Typography variant="body2">
+            IPFS CID:{" "}
+            <Link
+              href={`${process.env.NEXT_PUBLIC_IPFS_GET_URL}/${post.cid}`}
+              target="_blank"
+            >
+              {post.cid}
+            </Link>
+          </Typography>
+          <Typography variant="body2" mt={2}>
+            Category: {post.category}
+          </Typography>
+          <Typography variant="body2">By {post.authorAddress}</Typography>
+          <Typography variant="body2">{formatDate(post.createdAt)}</Typography>
+        </CardContent>
       </Card>
     </Container>
   );
